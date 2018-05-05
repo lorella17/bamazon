d99b5a58c10d758b8fd68e78f900103db60d812c
Leo Orellana <leoorellana@lawn-143-215-52-64.lawn.gatech.edu>
2018-05-05 10:04:56 -0400

    customer_total

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -64,13 +64,16 @@ function listItems() {
         connection.query("SELECT * FROM products", function (err, res) {
             if (err) throw err;
             console.log("=================");
-            console.log(res[0].stock_quantity);
+            // console.log(res[0].stock_quantity);
 
             answer.product = res[(answer.product - 1)].product_name;
             console.log(answer.product);
 
             for (let i = 0; i < res.length; i++) {
+                console.log(i);
+
                 if (answer.product === res[i].product_name) {
+                    console.log(res[i].stock_quantity);
 
                     //checking our stock and comparing it with user request
                     if (res[i].stock_quantity < answer.amount) {
@@ -79,8 +82,8 @@ function listItems() {
                     //if there is place the order and update the database
                     else if (res[i].stock_quantity > answer.amount) {
                         //then display the total cost
-                        checkOut((res[i].stock_quantity - answer.amount), answer.product);
-                        showTotal(answer.amount, res[i].price);
+                        checkOut((res[i].stock_quantity - answer.amount), answer.product, answer.amount, res[i].price);
+
 
                     } else {
                         console.log("not working");
@@ -92,11 +95,12 @@ function listItems() {
     });
 }
 
-function checkOut(quantity, product) {
+function checkOut(quantity, product, amount, price) {
+
     connection.query("UPDATE products SET ? WHERE ?",
         [
             {
-                stock_quanity: quantity
+                stock_quantity: quantity
             },
             {
                 product_name: product
@@ -105,14 +109,17 @@ function checkOut(quantity, product) {
 
         function (err, res) {
             if (err) throw err;
-            console.log("Thank you for shopping with us your order has been placed");
+            showTotal(amount, price);
+            //LINE 109 WILL RUN LAST OR AFTER THE CONNECTION QUERY
         });
+
 
 }
 
-function showTotal(quanity, price) {
-    total = quanity * price;
+function showTotal(quantity, price) {
+    total = quantity * price;
     console.log(`Your Total is: ${total}`);
-    return connection.end();
+    console.log("Thank you for shopping with us your order has been placed");
+    // return connection.end();
 }
 
